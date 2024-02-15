fff99c4d90b91669c116a81e288b5b09825c729e
adhikariprayag <adhikariprayag1@gmail.com>
2024-02-15 18:04:25 +0545

    pj upload

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -6,6 +6,7 @@ function Html() {
         <ul>
             <li><a href="https://bstrapdemo.netlify.app/" target='_blank'>https://bstrapdemo.netlify.app/</a></li>
             <li><a href="https://nepsop.netlify.app/" target='_blank'>https://nepsop.netlify.app/</a></li>
+            <li><a href="https://prayagadhikari.netlify.app/" target='_blank'>https://prayagadhikari.netlify.app/</a></li>
             <li><a href="/" >Home</a></li>
         </ul>
     </div>
