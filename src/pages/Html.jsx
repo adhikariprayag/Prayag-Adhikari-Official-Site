c67da435258c3e880c42df14f807c3e804aa2f2f
adhikariprayag <adhikariprayag1@gmail.com>
2024-03-18 20:13:23 +0545

    site additions

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -7,6 +7,9 @@ function Html() {
             <li><a href="https://bstrapdemo.netlify.app/" target='_blank'>https://bstrapdemo.netlify.app/</a></li>
             <li><a href="https://nepsop.netlify.app/" target='_blank'>https://nepsop.netlify.app/</a></li>
             <li><a href="https://prayagadhikari.netlify.app/" target='_blank'>https://prayagadhikari.netlify.app/</a></li>
+            <li><a href="https://ecotrand.vercel.app/" target='_blank'>https://ecotrand.vercel.app/</a></li>
+            <li><a href="https://bloggardemo.netlify.app/" target='_blank'>https://bloggardemo.netlify.app/</a></li>
+            <li><a href="https://grofarweb.netlify.app/" target='_blank'>https://grofarweb.netlify.app/</a></li>
             <li><a href="/" >Home</a></li>
         </ul>
     </div>
