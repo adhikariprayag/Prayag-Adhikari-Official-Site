5de9aa3183a66c4d52750764c6fa9988adb6b9a6
adhikariprayag <adhikariprayag1@gmail.com>
2024-03-31 22:53:29 +0545

    updates

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function Html() {
   return (
     <div>
-        <ul>
-            <li>1: <a href="https://bstrapdemo.netlify.app/" target='_blank'>https://bstrapdemo.netlify.app/</a></li>
-            <li>2: <a href="https://nepsop.netlify.app/" target='_blank'>https://nepsop.netlify.app/</a></li>
-            <li>3: <a href="https://prayagadhikari.netlify.app/" target='_blank'>https://prayagadhikari.netlify.app/</a></li>
-            <li>4: <a href="https://ecotrand.vercel.app/" target='_blank'>https://ecotrand.vercel.app/</a></li>
-            <li>5: <a href="https://deaxaut.netlify.app/" target='_blank'>https://deaxautt.netlify.app/</a></li>
-            <li>6: <a href="https://grofarweb.netlify.app/" target='_blank'>https://grofarweb.netlify.app/</a></li>
-            <li>7:<a href="https://bloggardemo.netlify.app/" target='_blank'>https://bloggardemo.netlify.app/</a></li>
-            <li>8:<a href="https://oganidemo.netlify.app//" target='_blank'>https://oganidemo.netlify.app/</a></li>
-            <li>9:<a href="https://prayagadh.netlify.app/" target='_blank'>https://prayagadh.netlify.app/</a></li>
+        <ul className='text-light'>
+            <li>1: <a href="https://prayagadh.netlify.app/" target='_blank'>https://prayagadh.netlify.app/</a></li>
+            <li>2: <a href="https://bstrapdemo.netlify.app/" target='_blank'>https://bstrapdemo.netlify.app/</a></li>
+            <li>3: <a href="https://nepsop.netlify.app/" target='_blank'>https://nepsop.netlify.app/</a></li>
+            <li>4: <a href="https://prayagadhikari.netlify.app/" target='_blank'>https://prayagadhikari.netlify.app/</a></li>
+            <li>5: <a href="https://ecotrand.vercel.app/" target='_blank'>https://ecotrand.vercel.app/</a></li>
+            <li>6: <a href="https://deaxaut.netlify.app/" target='_blank'>https://deaxautt.netlify.app/</a></li>
+            <li>7: <a href="https://grofarweb.netlify.app/" target='_blank'>https://grofarweb.netlify.app/</a></li>
+            <li>8: <a href="https://bloggardemo.netlify.app/" target='_blank'>https://bloggardemo.netlify.app/</a></li>
+            <li>9: <a href="https://oganidemo.netlify.app/" target='_blank'>https://oganidemo.netlify.app/</a></li>
             <li><a href="/" >Home</a></li>
         </ul>
     </div>
